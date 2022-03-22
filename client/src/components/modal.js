691c2354d6a19b3ab01571cd650a881aa097c8ee
JP-sDEV <jon.pablo80@gmail.com>
2022-03-22 17:51:36 -0400

    added table to display links

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Container, Paper, Typography } from '@mui/material';
 
 import { Form } from "./form.js"
 
 export const Modal = () => {
+    
     return (
         <Container Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
             <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
