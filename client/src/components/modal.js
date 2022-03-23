dbf9b96d26331e57eb1fe1fa495c49f7d72fc4ce
JP-sDEV <jon.pablo80@gmail.com>
2022-03-23 18:23:15 -0400

    Changed Modal tilte

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -9,9 +9,8 @@ export const Modal = () => {
         <Container Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
             <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
                 <Typography component="h1" variant="h4" align="center">
-                    hello
+                    Url Shortener
                 </Typography>
-
                 <Form />
             </Paper> 
         </Container>
