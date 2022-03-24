4fb46d3f3b421acc090ad90f4a95218caf867519
JP-sDEV <jon.pablo80@gmail.com>
2022-03-24 16:19:25 -0400

    added form control and styling

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -1,20 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { TextField, Button, FormControl } from '@mui/material';
 
 export const Form = () => {
 
     const [formData, setFormData] = useState({
-        fullUrl: null 
+        fullUrl: null,
+        submitBlock: true
     })
 
     const handleTextInput = (e) => {
         setFormData({
             ...formData, 
-            fullUrl: e.target.value
+            fullUrl: e.target.value,
+            submitBlock: false
         })
     }
 
-    const handleSubmit  = (e) => {
+    const handleSubmit  = () => {
 
         const urlForm = {
             "full": formData.fullUrl
@@ -32,16 +34,36 @@ export const Form = () => {
     
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <FormControl>
+            <form onSubmit={handleSubmit} style={{padding: "0 5%"}}>
+                <FormControl
+                    fullWidth
+                    margin="normal"
+                    sx={{ 
+                        display: "flex",
+                        flexDirection: "row"
+                        }}
+                    >
+
                     <TextField 
                         required
-                        fullWidth
                         label="Full Url"
+                        size="normal"
                         value = {formData.shortUrl}
+                        fullWidth
                         onChange = {(e) => handleTextInput(e)}
                     />
-                    <Button variant="outlined" type="submit" >Shorten</Button>
+
+                    <Button 
+                        variant="outlined"
+                        type="submit"
+                        sx={{ 
+                            ml: "1%"
+                            }}
+                        disabled={formData.submitBlock}
+                    >
+                        Shorten
+                    </Button>
+
                 </FormControl>
             </form>
         </div>
