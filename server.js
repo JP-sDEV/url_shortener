2a4a31a66b209079e1a7ce81db1b5a0d204c8e48
JP-sDEV <jon.pablo80@gmail.com>
2022-03-30 14:56:34 -0400

    changed production root

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ if (process.env.NODE_ENV === "production") {
     // Use static folder
     app.use(express.static('client/build'))
 
-    app.get("/", (req, res) => {
+    app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
     })
 }
