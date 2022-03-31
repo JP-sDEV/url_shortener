8a8c3ce9e11252a156a9f2c0c7c22db6e30ff46f
JP-sDEV <jon.pablo80@gmail.com>
2022-03-31 11:33:09 -0400

    added copy to clipboard button

--- client/src/components/customRows.js
+++ client/src/components/customRows.js
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import { TableCell, TableRow, Button } from '@mui/material';
-
  import { AppContext } from "../context"
 
  export const CustomRows = ({_id, full, short, clicks, created}) => {
      
     const {state: [state, setState]} = useContext(AppContext)
+
+    const PROD_URL = "https://shorty-urlshort.herokuapp.com"
 
     const handleShortUrlClick = (e) => {
         fetch(`/${e.target.value}`)
@@ -13,27 +14,32 @@ import { TableCell, TableRow, Button } from '@mui/material';
         .then(data =>  (window.open(data.url, "_blank")))
       } 
 
-      const handleDelete = (e) => {
+    const handleCopyToClipBoard = async(e) => {
+      await navigator.clipboard.writeText(`${PROD_URL}/get/${e.target.value}`);
+      alert(".Shorty Copied!");
+    }
 
-        const delForm = {
-          "id": e.target.value
-        }
-     
-        const requestOptions = {
-          method: "DELETE",
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(delForm)
-        }
-          fetch("/delUrl", requestOptions)
-          .then(res => res.json())
-          .then(data => setState({
-            ...state,
-            data: data.urls
-          }))
-          .catch((err) => {
-            console.log(err)
-          })
+    const handleDelete = (e) => {
+
+      const delForm = {
+        "id": e.target.value
       }
+    
+      const requestOptions = {
+        method: "DELETE",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(delForm)
+      }
+        fetch("/delUrl", requestOptions)
+        .then(res => res.json())
+        .then(data => setState({
+          ...state,
+          data: data.urls
+        }))
+        .catch((err) => {
+          console.error(err)
+        })
+    }
 
      return (
         <TableRow
@@ -51,6 +57,7 @@ import { TableCell, TableRow, Button } from '@mui/material';
             <Button onClick={(e) => handleShortUrlClick(e)} variant="text" value={short}>
                 https://{short}.shorty
             </Button>
+            <Button onClick={(e) => handleCopyToClipBoard(e)} value={short}>📋</Button>
             </TableCell>
 
             <TableCell align="center">{clicks}</TableCell>
