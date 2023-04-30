9faf51e2567e9e8bb53f656ef73df56c1374d45b
JP-sDEV <jon.pablo80@gmail.com>
2023-04-30 12:11:11 -0400

    fix: get proper property

--- server/config/passport.js
+++ server/config/passport.js
@@ -35,7 +35,7 @@ module.exports = function(passport) {
     }))
 
     passport.serializeUser((user, done) => {
-        done(null, user.id)
+        done(null, user._id)
     })
 
     passport.deserializeUser((id, done) => {
