fd607a621537645ba962fa175f88e8f836621cb6
JP-sDEV <jon.pablo80@gmail.com>
2023-05-02 14:37:14 -0400

    test: add cross-domain support

--- server/index.js
+++ server/index.js
@@ -31,6 +31,18 @@ app.use(session({
     })
 }))
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    if ('OPTIONS' == req.method) {
+         res.send(200);
+     } else {
+         next();
+     }
+    });
+
 // Passport
 require("./config/passport")(passport) 
 app.use(passport.initialize())
