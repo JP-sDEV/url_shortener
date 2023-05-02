b8529e046a9159e1b40de76d30ceb2b584977e89
JP-sDEV <jon.pablo80@gmail.com>
2023-05-02 13:51:59 -0400

    fix: callback to post

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,23 +8,23 @@ router.get("/google", passport.authenticate("google", { scope: ["profile"] }))
 
 // @desc Google auth callback
 // @route /auth/google/callback
-// router.get("/google/callback", passport.authenticate("google",
-//     {
-//         failureRedirect: "/login/fail",
-//         successRedirect: process.env.CLIENT_URL
-//     }),
-    
-//     )
-
-router.get(
-    "/google/callback",
-    passport.authenticate("google", {
-      failureRedirect: "/login/fail",
+router.post("/google/callback", passport.authenticate("google",
+    {
+        failureRedirect: "/login/fail",
+        successRedirect: process.env.CLIENT_URL
     }),
-    function (req, res) {
-      res.redirect(process.env.CLIENT_URL);
-    }
-  );
+    
+    )
+
+// router.get(
+//     "/google/callback",
+//     passport.authenticate("google", {
+//       failureRedirect: "/login/fail",
+//     }),
+//     function (req, res) {
+//       res.redirect(process.env.CLIENT_URL);
+//     }
+//   );
   
 
 
