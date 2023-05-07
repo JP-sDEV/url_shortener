6819468fc7fe1f77d8d3730495d5b766b01cc9cf
JP-sDEV <jon.pablo80@gmail.com>
2023-05-07 12:10:11 -0400

    refactor: add full url to cb

--- server/config/passport.js
+++ server/config/passport.js
@@ -7,7 +7,7 @@ module.exports = function(passport) {
         new GoogleStrategy({
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret:  process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "/auth/google/callback"
+            callbackURL: `${process.env.SERVER_URL}/auth/google/callback`
     },
     
     async (accessToken, refreshToken, profile, done) => {
