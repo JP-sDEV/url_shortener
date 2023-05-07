b1096d020050ac53818313cdefc0f6dfc763346e
JP-sDEV <jon.pablo80@gmail.com>
2023-05-07 11:50:35 -0400

    fix: remove static assets

--- server/index.js
+++ server/index.js
@@ -173,14 +173,14 @@ app.get("/get/:shortUrl", async(req, res) => {
 })
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
     
-    // Use static folder
-    app.use(express.static('client/build'))
+//     // Use static folder
+//     app.use(express.static('client/build'))
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-    })
-}
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+//     })
+// }
 
 app.listen(process.env.PORT || 5000, () => console.log("Server is up")) 
