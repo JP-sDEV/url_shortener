a393b09478ca544a726019ecab86d8f1734af33c
JP-sDEV <jon.pablo80@gmail.com>
2023-05-09 14:44:30 -0400

    .

--- server/config/passport.js
+++ server/config/passport.js
@@ -7,7 +7,8 @@ module.exports = function(passport) {
         new GoogleStrategy({
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret:  process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: `${process.env.SERVER_URL}/auth/google/callback`
+            callbackURL: `${process.env.SERVER_URL}/auth/google/callback`,
+            proxy: true
     },
     
     async (accessToken, refreshToken, profile, done) => {
