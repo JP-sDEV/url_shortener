2e2680003cd5af58c963cb7c2462a80fc52d82b7
JP-sDEV <jon.pablo80@gmail.com>
2024-10-10 14:48:08 -0400

    fix: add vercel to allowed domains

--- server/src/app.js
+++ server/src/app.js
@@ -38,6 +38,7 @@ app.use(
             maxAge: 1000 * 60 * 60 * 24 * 7, // one week
             httpOnly: process.env.NODE_ENV === 'production', // true if in production
             sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // None for cross-origin
+            domain: '.vercel.app',
         },
     })
 );
